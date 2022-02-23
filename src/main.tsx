2497afca253c7d14d827863ce12e6c78b05753f2
longlinguo <longlinguo@newrank.cn>
2022-02-23 16:40:45 +0800

    fix: 修改错误引入

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import './assets/index.less';
-import RoutesCofig from 'src/routes'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "./assets/index.less";
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-
       <App />
     </div>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
