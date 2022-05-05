d09bbd3b6852668c528fa3be79a9293720746d59
longlinguo <longlinguo@newrank.cn>
2022-05-05 11:08:17 +0800

    fix: 第二次提交

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "./assets/index.less";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+// sentry
+Sentry.init({
+  dsn: "http://29ba379f8e7947edac56cc1763892726@47.111.19.13:9000/3",
+  integrations: [new BrowserTracing()],
 
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+});
 ReactDOM.render(
   <React.StrictMode>
     <div>
